30202c309ff04be2ee4974e4900e0ff51247ed25
Emir Charles Repollo <ecgrepollo@gmail.com>
2020-03-08 17:37:51 +0800

    first commit.

---
+++ studentReviewer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace studentReviewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
